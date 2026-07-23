a783e871b64b87d3bcda4fc611b2f5621bfefa32
greciamaci <greciam@umich.edu>
2026-07-23 13:27:25 -0400

    Make spelling correction

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ from qdrant_client.models import Distance, VectorParams, PointStruct
 # =========================
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using device:", device)
-print("**** delete after successful deployment test")
 
 # =========================
 # HF Token
