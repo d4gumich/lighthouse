5e4415477d5e2efbe1c2075da3b1ba30f8ca5331
greciamaci <greciam@umich.edu>
2026-07-23 11:14:45 -0400

    Change print statement

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from qdrant_client.models import Distance, VectorParams, PointStruct
 # =========================
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using device:", device)
-print("**** delete after successful deployment test")
+print("****delete after successful deployment test")
 
 # =========================
 # HF Token
